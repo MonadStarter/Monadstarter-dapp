4f8cd8d833d635898a1d5a26f365b308589834d0
moandad <moandad202@gmail.com>
2024-08-07 23:31:12 -0400

    testing commit issue

--- packages/foundry/contracts/ftm_contracts/Staker.sol
+++ packages/foundry/contracts/ftm_contracts/Staker.sol
@@ -5,7 +5,7 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-
+//testing
 import {IStaker} from "./IStaker.sol";
 //TODO: remove console
 import "forge-std/console.sol";
