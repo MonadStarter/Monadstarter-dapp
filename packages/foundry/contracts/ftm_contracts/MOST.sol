4497cfef24a9a1f9a27235b87306450a00ece8af
Richard Martinez <133671180+richardzks@users.noreply.github.com>
2024-08-08 12:51:29 +0530

    token version update

--- packages/foundry/contracts/ftm_contracts/MOST.sol
+++ packages/foundry/contracts/ftm_contracts/MOST.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.26;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MOST is ERC20 {
-    constructor(uint256 initialSupply) ERC20("ZK Starter", "ZKSTR") {
+    constructor(uint256 initialSupply) ERC20("MOST", "MonadStarter") {
         _mint(msg.sender, initialSupply);
     }
 }
